8dfddc8ce67e4be971a809d757b1152d47380f71
SagarGalle <sagargalle@gmail.com>
2014-10-28 11:48:17 -0700

    Score.java

---
+++ src/main/java/org/mule/leagues/request/Score.java
@@ -0,0 +1,35 @@
+
+package org.mule.leagues.request;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@JsonAutoDetect
+@XmlRootElement(namespace = "http://mulesoft.com/schemas/soccer")
+public class Score {
+    private int homeTeamScore;
+    private int awayTeamScore;
+
+    @JsonProperty
+    @XmlElement(required = true, namespace = "http://mulesoft.com/schemas/soccer")
+    public int getHomeTeamScore() {
+        return homeTeamScore;
+    }
+
+    public void setHomeTeamScore(int homeTeamScore) {
+        this.homeTeamScore = homeTeamScore;
+    }
+
+    @JsonProperty
+    @XmlElement(required = true, namespace = "http://mulesoft.com/schemas/soccer")
+    public int getAwayTeamScore() {
+        return awayTeamScore;
+    }
+
+    public void setAwayTeamScore(int awayTeamScore) {
+        this.awayTeamScore = awayTeamScore;
+    }
+}
